f2c5be6dc152b71db14ea12a75e5a3b36771bcd4
Cong Wang <cong.wang@sendgrid.com>
2020-04-23 13:57:50 -0700

    added arrays.

--- basics/arrays.go
+++ basics/arrays.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/**
+  要知道数组个数
+  很麻烦
+  go语言一般用slice
+*/
 func main() {
 	var arr1 [5]int
 	arr2 := [3]int{1, 3, 5}
@@ -11,4 +16,35 @@ func main() {
 
 	var grid [4][5]bool
 	fmt.Println(grid)
+
+	// print array
+	for i, v := range arr3 {
+		fmt.Println(i, " ", v)
+	}
+
+	printArray1(arr1)
+	for i, v := range arr1 {
+		fmt.Println(i, " ", v)
+	}
+
+	printArray2(&arr1)
+	for i, v := range arr1 {
+		fmt.Println(i, " ", v)
+	}
+}
+
+// 每次都是要把数组copy一次
+func printArray1(arr [5]int) {
+	arr[0] = 100
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+// 可以改变原来的数组里面的值了
+func printArray2(arr *[5]int) {
+	arr[0] = 99
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
 }
