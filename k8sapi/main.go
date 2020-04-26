1e89eb50258c471be8e3a5c2137ec6f6ff34c432
Cong Wang <cong.wang@sendgrid.com>
2020-04-25 19:11:16 -0700

    added k8sapi directory.

---
+++ k8sapi/main.go
@@ -0,0 +1,76 @@
+// Note: the example only works with the code within the same release/branch.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	var (
+		kubeconfig *string
+		pod        v1.Pod
+	)
+
+	kubeconfig = flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	podList, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(podList.Items))
+
+	for _, pod = range podList.Items {
+		fmt.Printf("Pod %d\n", pod.Status.Phase)
+	}
+
+	// 2. watch for pod delete event
+	watchlist := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", v1.NamespaceDefault,
+		fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Pod{},
+		time.Second*0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				fmt.Printf("add: %s \n", obj)
+			},
+			DeleteFunc: func(obj interface{}) {
+				fmt.Printf("delete: %s \n", obj)
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
+			},
+		},
+	)
+	stop := make(chan struct{})
+	go controller.Run(stop)
+	for {
+		time.Sleep(time.Second)
+	}
+
+}
