d1e6da91d3f1e66b4afab546d34d109ac8b7897d
Cong Wang <cong.wang@sendgrid.com>
2020-04-26 23:55:54 -0700

    added demo8

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 
 ## k8s-client-go-examples
 
-* Demo1 : connect to k8s and list all available pods.
-* Demo2
-* Demo3
-* Demo4
-* Demo5
-* Demo6
-* Demo7 : get logs from POD. same as running command : k logs <pod_id>
+* Demo 1 : connect to k8s and list all available pods.
+* Demo 2
+* Demo 3
+* Demo 4
+* Demo 5
+* Demo 6
+* Demo 7 : get logs from POD. same as running command : k logs <pod_id>
+* Demo 8 : 
---
+++ k8s-client-go-examples/demo8/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/tools/cache"
+
+	"congwang.com/learnGo/k8s-client-go-examples/common"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func main() {
+	var (
+		clientset *kubernetes.Clientset
+		err       error
+	)
+
+	// init k8s client
+	if clientset, err = common.InitClient(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// watch for pod changes.
+	watchlist := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", v1.NamespaceDefault, fields.Everything())
+	_, controller := cache.NewInformer(
+		watchlist,
+		&v1.Pod{},
+		time.Second*0,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				fmt.Printf("add: %s \n", obj.(*v1.Pod).Name)
+			},
+			DeleteFunc: func(obj interface{}) {
+				fmt.Printf("deleted: %s \n", obj.(*v1.Pod).Name)
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				fmt.Printf("old: %s, new: %s \n", oldObj.(*v1.Pod).Name, newObj.(*v1.Pod).Name)
+			},
+		},
+	)
+	stop := make(chan struct{})
+	go controller.Run(stop)
+	for {
+		time.Sleep(time.Second)
+	}
+
+}
